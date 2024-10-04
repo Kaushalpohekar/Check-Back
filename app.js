397bb65be495f2056021c7c94a52b7c14ff8b6a1
Kaushal <kaushalpohekar1@gmail.com>
2024-10-04 16:20:20 +0530

    Kaushak

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const router = require('./routes');
-const { startCronJobs } = require('./Cronjobs/shift');
+// const { startCronJobs } = require('./Cronjobs/shift');
 
 const app = express();
 const port = 4000;
